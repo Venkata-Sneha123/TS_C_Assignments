01f8093a73bd0f2b2df2f170f80925dca7a901f7
K. Venkata Sneha <98574108+Venkata-Sneha123@users.noreply.github.com>
2022-05-17 10:52:44 +0530

    insertion sort technique implementation

---
+++ Sorting/insertion.c
@@ -0,0 +1,55 @@
+/* WAP to implement insertion sort. */
+
+#include<stdio.h>
+
+void insertion(int a[],int size);
+
+int main()
+{
+	int size;
+	printf("Enter a size of an array: ");
+	scanf("%d",&size);
+
+	int i,arr[size];
+	printf("Enter an array :\n");
+
+	for(i=0;i<size;i++)
+		scanf("%d",&arr[i]);
+	insertion(arr,size);
+	return 0;
+}
+
+void insertion(int a[],int size)
+{
+	int i,j,flag,temp;
+	for(i=0;i<size;i++)
+	{	
+		temp=a[i];
+		j=i-1;
+		while(j>=0 && a[j]>temp)
+		{
+			a[j+1]=a[j];
+			j--;
+		}
+		a[j+1]=temp;
+	}
+
+	printf("The sorted array is : ");
+
+	for(i=0;i<size;i++)
+	{
+		printf("%d ",a[i]);
+	}
+	printf("\n");
+}
+/*
+output:
+Enter a size of an array: 5
+Enter an array :
+6
+4
+2
+5
+1
+The sorted array is : 1 2 4 5 6 
+*/

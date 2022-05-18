5facb3c2f7014403a1f8dfc725958be1885c1655
K. Venkata Sneha <98574108+Venkata-Sneha123@users.noreply.github.com>
2022-05-18 12:45:40 +0530

    print the star pattern

--- Patterns/star_pattern15.c
+++ Patterns/star_pattern15.c
@@ -14,35 +14,39 @@ The pattern is:
 #include<stdio.h>
 int main()
 {
-int i,j,k,n=5;
-printf("The pattern is:\n");
-for(i = 1;i <= n; i++)
-{
-	for(k = 1;k <= n-i; k++)
+    
+    int i,j,l,k,n=5;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n-i;j++)
         {
-        	printf("  ");
+            printf(" ");
         }
-        for(j = 1; j <= (i*2)-1; j++)
+        for(k=0;k<=i;k++)
         {
-                printf("*");
-                printf(" ");
-
+            printf("*");
+        }
+        for(l=0;l<i;l++)
+        {
+            printf("*");
         }
         printf("\n");
-        }
-        i=i-2;
-        for(i = i;i >= 1; i--)
-        {
-            for(k = n-i;k >= 1; k--)
-            {
-                printf("  ");
-            }
-            for(j = (i*2)-1;j >= 1; j--)
-            {
-                printf("*");
-                printf(" ");
-
-            }
-            printf("\n");
-        }
     }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+            printf(" ");
+        }
+        for(k=0;k<n-i;k++)
+        {
+            printf("*");
+        }
+        for(l=0;l<n-i-1;l++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}

875fd25b555f5283353aa37a5f65207fa3f9b691
K. Venkata Sneha <98574108+Venkata-Sneha123@users.noreply.github.com>
2022-05-18 12:42:24 +0530

    print the star pattern

--- Patterns/star_pattern11.c
+++ Patterns/star_pattern11.c
@@ -8,21 +8,25 @@ Enter the number of rows: 5
 
 */
 
-#include <stdio.h>
-int main() 
+#include<stdio.h>
+int main()
 {
-int rows, i, j, space;
-printf("Enter the number of rows: ");
-scanf("%d", &rows);
-for (i = rows; i >= 1; --i) 
-{
-	for (space = 0; space < rows - i; ++space)
-        	printf("  ");
-      	for (j = i; j <= 2 * i - 1; ++j)
-         	printf("* ");
-      	for (j = 0; j < i - 1; ++j)
-         	printf("* ");
+    int i,j,k,l,n=5;
+    for(i=0;i<n;i++)
+    {
+        for(k=0;k<i;k++)
+        {
+            printf(" ");
+        }
+        for(j=0;j<n-i;j++)
+        {
+            printf("*");
+        }
+        for(l=0;l<n-i-1;l++)
+        {
+            printf("*");
+        }
         printf("\n");
-}
-return 0;
+    }
+    return 0;
 }
